6f0b3b47eaf42cd59dd0548067c65ef28477eff5
Kārlis Ruža <ruza.karlis@gmail.com>
2019-05-05 13:25:56 +0300

    Temp

--- src/Routes/Profile/Profile.js
+++ src/Routes/Profile/Profile.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
-import Layout from '../../Components/Layout/Layout.js'
+import Layout from '../../Components/Layout/Layout.js';
+import Container from '../../Components/Layout/Container/Container.js';
 
 class Profile extends React.Component{
     render(){
         return(
             <Layout>
-                <div>user profile</div>
+                <Container>
+                    <div className='profile__head'>
+                        <img src='/' alt='profile pic' />
+                        <p className='bio'> this be description </p>
+                    </div>
+                    {/* <Tabs>
+                        <Tab title='Packs'>
+                            <div>packs</div>
+                        </Tab>
+                        <Tab title='Likes'>
+                            <div>Likes</div>
+                        </Tab>
+                        <Tab title='Settings'>
+                            <div>Settings</div>
+                        </Tab>
+                    </Tabs> */}
+                </Container>
             </Layout>
         );
     }
