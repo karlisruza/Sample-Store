b00154d7ef699166022daf3d7633eea623a355c3
Kārlis Ruža <ruza.karlis@gmail.com>
2019-05-10 10:28:00 +0300

    continued work on schema

--- src/Data/Schema/Schema.js
+++ src/Data/Schema/Schema.js
@@ -8,6 +8,7 @@ const db = {};
 db.conn = pgp(Config.DBConfig);
 
 const {
+    GraphQLList,
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
@@ -15,8 +16,19 @@ const {
     GraphQLBoolean,
     GraphQLInt
  } = graphql;
-
  const { GraphQLDateTime } = isoDate;
+
+ function userResolve(parentValue, args) {
+    console.log(parentValue.user_id);
+    const query = `SELECT * FROM "users" WHERE user_id='${parentValue.user_id}'`;
+    return db.conn.one(query)
+        .then(data => {
+            return data;
+        })
+        .catch(err => {
+            return 'error: ', err;
+        });
+}
 
  const UserType = new GraphQLObjectType({
      name: "User",
@@ -39,9 +51,45 @@ const {
         name: { type: GraphQLString },
         user_id: {
             type: UserType,
-            resolve(parentValue, args) {
-                console.log(parentValue.user_id);
-                const query = `SELECT * FROM "users" WHERE user_id='${parentValue.user_id}'`;
+            resolve(parentValue, args){
+               return userResolve(parentValue, args); 
+            }
+        },
+        price: { type: GraphQLInt },
+        community: { type: GraphQLBoolean },
+        created_on: { type: GraphQLDateTime },
+        demo_path: { type: GraphQLString },
+        img_path: { type: GraphQLString },
+     })
+ })
+
+ const SampleType = new GraphQLObjectType({
+     name: "Sample",
+     fields: () =>({
+         sample_id: { type: GraphQLID },
+         name: { type: GraphQLString },
+         user_id: {
+             type: UserType,
+             resolve(parentValue, args){
+                return userResolve(parentValue, args); 
+             }
+         },
+         price: { type: GraphQLInt },
+         created_on: { type: GraphQLDateTime },
+         sample_path: { type: GraphQLString },
+         key: { type: GraphQLString },
+         bpm: { type: GraphQLInt }
+     })
+ })
+
+ const PackSamplesType = new GraphQLObjectType({
+     name: "PackSample",
+     fields: () =>({
+        id: { type: GraphQLID },
+        sample_id:{
+            type: SampleType,
+            resolve(parentValue, args){
+                const query = `SELECT * FROM "samples" WHERE sample_id='${parentValue.sample_id}'`;
                 return db.conn.one(query)
                     .then(data => {
                         return data;
@@ -51,11 +99,57 @@ const {
                     });
             }
         },
-        price: { type: GraphQLInt },
-        community: { type: GraphQLBoolean },
-        created_on: { type: GraphQLDateTime },
-        demo_path: { type: GraphQLString },
-        img_path: { type: GraphQLString },
+        pack_id: {
+            type: PackType,
+            resolve(parentValue, args){
+                const query = `SELECT * FROM "packs" WHERE pack_id='${parentValue.pack_id}'`;
+                return db.conn.one(query)
+                    .then(data => {
+                        return data;
+                    })
+                    .catch(err => {
+                        return 'error: ', err;
+                    });
+            }
+        }
+        })
+ })
+ const LikesType = new GraphQLObjectType({
+     name: "Likes",
+     fields: () =>({
+        id: { type: GraphQLID },
+        sample_id:{
+            type: SampleType,
+            resolve(parentValue, args){
+                const query = `SELECT * FROM "samples" WHERE sample_id='${parentValue.sample_id}'`;
+                return db.conn.one(query)
+                    .then(data => {
+                        return data;
+                    })
+                    .catch(err => {
+                        return 'error: ', err;
+                    });
+            }
+        },
+        pack_id: {
+            type: PackType,
+            resolve(parentValue, args){
+                const query = `SELECT * FROM "packs" WHERE pack_id='${parentValue.pack_id}'`;
+                return db.conn.one(query)
+                    .then(data => {
+                        return data;
+                    })
+                    .catch(err => {
+                        return 'error: ', err;
+                    });
+            }
+        },
+        user_id: {
+            type: UserType,
+            resolve(parentValue, args){
+               return userResolve(parentValue, args); 
+            }
+        }
      })
  })
 
@@ -81,7 +175,7 @@ const {
              type: PackType,
              args: { id: { type: GraphQLID }},
              resolve(parentValue, args){
-                const query = `SELECT * FROM "packs"`;
+                const query = `SELECT * FROM "packs" WHERE id=${args.id}`;
                 return db.conn.one(query)
                 .then(data => {
                     return data;
@@ -90,7 +184,60 @@ const {
                     return 'error: ', err;
                 });
              }
-         }
+         },
+         packs: {
+            type: new GraphQLList(PackType),
+            resolve(parentValue, args){
+                    const query = `SELECT * FROM "packs"`;
+                    return db.conn.one(query)
+                    .then(data => {
+                        return data;
+                    })
+                    .catch(err => {
+                        return 'error: ', err;
+                    });
+            }
+        },
+         sample: {
+            type: SampleType,
+            args: { id: { type: GraphQLID}},
+            resolve(){
+                const query = `SELECT * FROM "samples"`;
+                return db.conn.one(query)
+                .then(data => {
+                    return data;
+                })
+                .catch(err => {
+                    return 'error: ', err;
+                });                
+            }
+         },
+         packsample: {
+             type: PackSamplesType,
+             resolve(){
+                const query = `SELECT * FROM "packsamples"`;
+                return db.conn.one(query)
+                .then(data => {
+                    return data;
+                })
+                .catch(err => {
+                    return 'error: ', err;
+                });                
+            }
+         },
+         likes: {
+            type: LikesType,
+            resolve(){
+               const query = `SELECT * FROM "likes"`;
+               return db.conn.one(query)
+               .then(data => {
+                   return data;
+               })
+               .catch(err => {
+                   return 'error: ', err;
+               });                
+           }
+       },
      }
  })
 
